7214a484e346b21c94c976afa1b332aab36f6b41
anj0la <anjola.aina@gmail.com>
2024-04-03 18:54:19 -0600

    init

---
+++ cbow.py
@@ -0,0 +1,49 @@
+import torch 
+import pandas as pd
+
+# Data Preparation (may not need all functions)
+def process_data(data):
+    pass
+
+def remove_special_characters(text):
+    pass
+
+def convert_to_lowercase(text):
+    pass
+
+def tokenize_sentences(text):
+    pass
+
+def create_vocabulary(tokenized_sentences):
+    pass
+
+# Generate Training Data (may not need all functions)
+def generate_training_data(processed_documents):
+    pass
+
+# Training the model
+class CBOW():
+    def __init__(self, vocab_size, embedding_dim):
+        pass
+    
+    def forward(self):
+        pass
+
+    def fit(self):
+        pass
+
+def visualize_word_vectors(word_vectors):
+    pass
+
+def analyze_visualization(data):
+    pass
+
+# After Training
+def visualize_word_vectors(word_vectors):
+    pass
+
+def dimensionality_reduction_with_pca(word_vectors):
+    pass
+
+def generate_scatter_plot(data):
+    pass
